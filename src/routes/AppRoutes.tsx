8888eef40fca332ba7fbb0dbc58abcc94465b10e
Fiayz <sifathhossain115@gmail.com>
2025-08-10 17:27:34 +0600

    Fix: Home route fixed

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-// import Home from "../components/pages/Home";
+import Home from "../components/pages/Home";
 import AddNews from "../components/pages/AddNews";
 import ManageNews from "../components/pages/ManageNews";
 import NewsDetails from "../components/pages/NewsDetails";
@@ -28,6 +28,7 @@ export default function AppRoutes() {
           </PrivateRoute>
         }
       />
+      <Route path="*" element={<Home />} />
       <Route path="/manage" element={<ManageNews />} />
       <Route path="/news/:id" element={<NewsDetails />} />
       <Route path="/login" element={<Login />} />
