220f440cd49dad5fac9fa85d19e6a7a6724d4239
Fiayz <sifathhossain115@gmail.com>
2025-08-10 17:17:06 +0600

    Fix: AppRoutes updated for testing

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,33 +1,33 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "../components/pages/Home";
+// import Home from "../components/pages/Home";
 import AddNews from "../components/pages/AddNews";
 import ManageNews from "../components/pages/ManageNews";
 import NewsDetails from "../components/pages/NewsDetails";
 import Login from "../components/pages/Login";
 import Register from "../components/pages/Register";
-// import PrivateRoute from "./PrivateRoute";
+import PrivateRoute from "./PrivateRoute";
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      {/* <Route
+      {/* <Route path="/" element={<Home />} /> */}
+      <Route
         path="/add"
         element={
           <PrivateRoute>
             <AddNews />
           </PrivateRoute>
         }
-      /> */}
-      <Route path="/add" element={<AddNews />} />
-      {/* <Route
+      />
+      {/* <Route path="/add" element={<AddNews />} /> */}
+      <Route
         path="/manage"
         element={
           <PrivateRoute>
             <ManageNews />
           </PrivateRoute>
         }
-      /> */}
+      />
       <Route path="/manage" element={<ManageNews />} />
       <Route path="/news/:id" element={<NewsDetails />} />
       <Route path="/login" element={<Login />} />
